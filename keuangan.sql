0edcba237195ad6b797c0b5c288f64981e914608
Hudafajral <161544519+Hudafajral@users.noreply.github.com>
2025-12-14 20:28:50 +0700

    Clean up keuangan.sql by removing comments
    Removed unnecessary SQL comments and character set settings.

--- keuangan.sql
+++ keuangan.sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 05, 2025 at 10:09 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `keuangan`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaksi`
---
 
 CREATE TABLE `transaksi` (
   `id` bigint(20) UNSIGNED NOT NULL,
@@ -35,35 +15,16 @@ CREATE TABLE `transaksi` (
   `jumlah` decimal(12,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `transaksi`
---
+
 
 INSERT INTO `transaksi` (`id`, `tanggal`, `keterangan`, `jenis`, `jumlah`) VALUES
 (1, '2025-12-01', 'gajian', 'masuk', 10000000.00),
 (2, '2025-12-01', 'belanja', 'keluar', 1000000.00);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `transaksi`
---
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `transaksi`
---
 ALTER TABLE `transaksi`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
